e2956b97614251cf176f38c00fa506e472bac7a7
Anders Helbo <anders.helbo@me.com>
2024-04-03 10:51:03 +0200

    pallette and weak D

--- LOCAL_HENDRIX/process_output.sh
+++ LOCAL_HENDRIX/process_output.sh
@@ -3,19 +3,29 @@
 # Start timer
 start=$(date +%s)
 
-python3 clear_outdated_files.py "/Users/andershelbo/Desktop/hendrix-folder/output"
+#define source paths:
+P2P_CYCLEGAN="hendrix:~/pix2pix_cycleGan/checkpoints/*"
+WEAK_P2P="hendrix:~/pix2pix_weak_D/checkpoints/*"
+#define target path (output folder):
+TARGET_DIR="/Users/andershelbo/Desktop/MRI2CT/LOCAL_HENDRIX/output"
+
+python3 clear_outdated_files.py $TARGET_DIR
 echo ""
 
 # scp -r hendrix:~/pix2pix_cycleGan/checkpoints/* /Users/andershelbo/Desktop/hendrix-folder/output/p2p_cyc
 # rsync -av --ignore-existing hendrix:~/pix2pix_cycleGan/checkpoints/* /Users/andershelbo/Desktop/hendrix-folder/output/p2p_cyc
 
-rsync -zarv --include="*/" --include="*.txt" --include="*.html" --include="*.png" --exclude="*" "hendrix:~/pix2pix_cycleGan/checkpoints/*" "/Users/andershelbo/Desktop/hendrix-folder/output/p2p_cyc"
+rsync -zarv --include="*/" --include="*.txt" --include="*.html" --include="*.png" --exclude="*" $P2P_CYCLEGAN "$TARGET_DIR/p2p_cyc"
+rsync -zarv --include="*/" --include="*.txt" --include="*.html" --include="*.png" --exclude="*" $WEAK_P2P "$TARGET_DIR/p2p_weak"
 
-sleep 5
+# rsync -zarv --include="*/" --include="*.txt" --include="*.html" --include="*.png" --exclude="*" --files-from=$P2P_CYCLEGAN --files-from=$WEAK_P2P "$TARGET_DIR/"
 
-python3 plot_losses.py "/Users/andershelbo/Desktop/hendrix-folder/output"
 
-python3 html2png.py "/Users/andershelbo/Desktop/hendrix-folder/output"
+sleep 3
+
+python3 plot_losses.py $TARGET_DIR
+
+python3 html2png.py $TARGET_DIR
 
 
 # End time
