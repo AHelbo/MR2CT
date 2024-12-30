f1d8edce280fe06d78ab6b305e671f6682ce48b5
SirHelbo <89070425+SirHelbo@users.noreply.github.com>
2024-12-30 22:43:54 +0100

    debug

--- slurm/palette_lr1e-4_nc1.sh
+++ slurm/palette_lr1e-4_nc1.sh
@@ -29,6 +29,6 @@ echo "Using Python from: $(which python3)"
 echo "Using pip from: $(which pip)"
 
 # Ensure Pillow is installed in the virtual environment
-/home/pjf246/diffusion/diffusion_env/bin/python3 -m pip show Pillow
+~/diffusion/diffusion_env/bin/python3 -m pip show Pillow
 
-/home/pjf246/diffusion/diffusion_env/bin/python3 run.py -p train -c config/mr2ct_lr1e-4_nc1.json
+~/diffusion/diffusion_env/bin/python3 run.py -p train -c config/mr2ct_lr1e-4_nc1.json
