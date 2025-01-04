4061ae8f0df371aa6924d0d5398ccca645977e4d
SirHelbo <89070425+SirHelbo@users.noreply.github.com>
2025-01-05 00:55:06 +0100

    old fstring

--- datasets/src/verify_dataset.py
+++ datasets/src/verify_dataset.py
@@ -12,24 +12,24 @@ def verify_AB(A_dir, B_dir):
     for elm in [elm for elm in os.listdir(B_dir) if elm.split(".")[-1] == "tiff"]:
         Bs.add(elm)        
 
-    print_hierarchical(f"{len(As) = }",2)
-    print_hierarchical(f"{len(Bs) = }",2)
-    print_hierarchical(f"{len(As - Bs) = }",2)
+    print_hierarchical(f"len(As) = {len(As)}", 2)
+    print_hierarchical(f"len(Bs) = {len(Bs)}", 2)
+    print_hierarchical(f"len(As - Bs) = {len(As - Bs)}", 2)
 
     return As == Bs
 
 def verify_dataset():
     success = True
     for split in ["train", "val", "test"]:
-        print_hierarchical(f"Verifying {split} data:",1)
+        print_hierarchical(f"Verifying {split} data:", 1)
         A_dir = os.path.join(OUTPUT_DIR, split, "A")
         B_dir = os.path.join(OUTPUT_DIR, split, "B")
         success = success and verify_AB(A_dir, B_dir)
 
-    if (success):
-        print_hierarchical("Verification completed succesfully!",1)
+    if success:
+        print_hierarchical("Verification completed successfully!", 1)
     else:
-        print_hierarchical("Verification failed, some data is misaligned.",1)
+        print_hierarchical("Verification failed, some data is misaligned.", 1)
 
 def main():
     verify_dataset()
