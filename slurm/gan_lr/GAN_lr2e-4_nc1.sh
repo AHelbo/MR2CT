378d0d305374ff6c6cc9c4de1cc25bce55dafb0b
SirHelbo <89070425+SirHelbo@users.noreply.github.com>
2025-01-11 13:57:48 +0100

    added more experiements for gan

--- slurm/gan_lr/GAN_lr2e-4_nc1.sh
+++ slurm/gan_lr/GAN_lr2e-4_nc1.sh
@@ -22,4 +22,4 @@ echo "Using pip from: $(which pip)"
 echo "RUNNING SCRIPT:"
 
 #Run training steps
-python3 train.py --name GAN_dfreq_nc1 --dataroot ../datasets/mri2ct_nc1 --lambda_L1 100 --batch_size 10 --lr 0.0002
+python3 train.py --name GAN_lr2e-4_nc1 --dataroot ../datasets/mri2ct_nc1 --lambda_L1 100 --batch_size 10 --lr 0.0002
